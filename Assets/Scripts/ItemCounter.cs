405e8a2ee88ecdaf4d17b43ddd0cbe009be49bb7
artkochnev <100959553+artkochnev@users.noreply.github.com>
2022-08-26 15:52:41 +0200

    scripts

--- Assets/Scripts/ItemCounter.cs
+++ Assets/Scripts/ItemCounter.cs
@@ -13,7 +13,7 @@ public class ItemCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (counter > 0) {
+        if (counter > 4) {
             //load next scene
             SceneManager.LoadScene("Title Screen");
         } 
